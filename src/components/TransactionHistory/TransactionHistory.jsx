8be9af6ff5604b1550b75a77bece2488651b0188
Konstantin Rybakov <konstantin.rybakov@nmi.com>
2024-11-30 15:09:43 +0200

    fix styles for vercel

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,8 +1,8 @@
-import "./TransactionHistory.module.css";
+import css from "./TransactionHistory.module.css";
 
 export default function TransactionHistory({ transactions }) {
   return (
-    <table>
+    <table className={css.table}>
       <thead>
         <tr>
           <th>Type</th>
